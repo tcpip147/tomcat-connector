4f774cb2223416fef697934e4aec0be4e80a4258
tcpip147 <tcpip147@gmail.com>
2024-12-08 20:59:28 +0900

    change version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.tcpip147"
-version = "1.0-SNAPSHOT"
+version = "1.0-RELEASE"
 
 repositories {
   mavenCentral()
@@ -32,7 +32,6 @@ tasks {
 
   patchPluginXml {
     sinceBuild.set("232")
-    untilBuild.set("242.*")
   }
 
   signPlugin {
